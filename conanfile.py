8203a3fdcaf29bf680b7075f70a595dd3ebb655d
Javier G. Sogo <jgsogo@gmail.com>
2019-03-22 19:19:23 +0100

    trial: private only if static, otherwise dependency hell

--- conanfile.py
+++ conanfile.py
@@ -15,8 +15,18 @@ class Tileset(ConanFile):
            "revision": "auto"}
 
     def requirements(self):
-        self.requires("rapidxml/1.13@bincrafters/stable", private=True)
-        self.requires("boost/1.69.0@conan/stable", private=True)
+        # TODO: Conan, default_options are not being assigned yet
+        rapidxml_shared = self.options["rapidxml"].shared
+        boost_shared = self.options["boost"].shared
+        
+        public_boost = boost_shared or boost_shared is None
+        public_rapidxml = rapidxml_shared or rapidxml_shared is None
+        
+        self.output.info("Require rapidxml public {}".format(public_boost))
+        self.output.info("Require boost public {}".format(public_rapidxml))
+
+        self.requires("rapidxml/1.13@bincrafters/stable", private=not public_boost)
+        self.requires("boost/1.69.0@conan/stable", private=not public_rapidxml)
 
     def build(self):
         cmake = CMake(self)
